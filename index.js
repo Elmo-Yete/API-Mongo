8e2564cbadd9c7a6ccda9a802c196e0a7048ff6c
DamianT <elmo@DESKTOP-OP1RDAI>
2023-06-08 20:54:07 -0600

    err:check catch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json())
 
 // generar un objeto con estructura y se le pasa a mongo
 const koderSchema = new mongoose.Schema({
@@ -59,8 +60,66 @@ app.get ("/",(req,res)=> {
     res.json("Estamos en el home")
 })
 
-app.get ("/koders", async (req,res)=> {
-    // acceder a la bd
-    const koders = await Koder.find();
-    console.log("koders",koders)
+
+app.get ("/koders", async (request,response)=> {
+    try {
+        // const {name} = request.query
+        // acceder a la bd
+        const koders = await Koder.find(request.query);
+        // console.log("koders",koders)
+        response.json({
+            succes:true,
+            data:koders
+        })
+    }catch(error) {
+        response.status(400)
+        response.json({
+            succes:false,
+            message:error.message
+        })
+    }
+})
+
+app.post("/koders", async (request,response)=> {
+try {
+// console.log("request:",request.body)
+    const koder = await  Koder.create(request.body)
+    response.status(201);
+    response.json ({
+        succes:true,
+        data:koder
+    })
+} catch (error) {
+    response.status(400)
+    response.json ({
+        succes:false,
+        message:error.message
+    })
+}
+})
+
+app.get ("/koders/:id", async (request,response)=> {
+    const {id} = request.params
+    try {
+        const koder = await Koder.findById(id);
+        response.status(200)
+        response.json ({
+            succes:true,
+            data:koder
+        })
+    } catch (error) {
+        if (id.length < 8) {
+            response.status(400)
+            response.json ({
+                succes:false,
+                message:"El id es demasiado corto, vuelva a intentar"
+            })
+        } else {
+            response.status(418) // 404
+            response.json({
+                succes:false,
+                message:"El koder que buscabas no existe"
+            })
+        }
+    }
 })
