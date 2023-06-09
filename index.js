3273c86d4046044e638b3b0b70f1f325995e79a4
DamianT <elmo@DESKTOP-OP1RDAI>
2023-06-08 22:58:07 -0600

    err:update method half

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { options } = require("nodemon/lib/config");
 
 const app = express();
+
+// * primer middleware
 app.use(express.json())
 
-// generar un objeto con estructura y se le pasa a mongo
+// TODO: generar un objeto con estructura y se le pasa a mongo
 const koderSchema = new mongoose.Schema({
     name: {
         type:String,
@@ -36,13 +39,21 @@ const koderSchema = new mongoose.Schema({
     }
 })
 
-// modelo  (va en mayus y siempre es en singular )
+// ! modelo  (va en mayus y siempre es en singular )
 
 const Koder = mongoose.model("Koders", koderSchema, "Koders");
 
-
-
-//  conectar BD
+// ? middleware de prueba con obj date
+app.use((req, res, next) => {
+    console.log('Time:', Date.now())
+    next()
+})
+// ? midd encapsulado 
+const capsuladeMidd = (req,res,next) => {
+    console.log("soy un mid encapsulado en una const")
+    next()
+}
+// ! conectar BD
 const URL = "mongodb+srv://ElmoYete:MWXfjrtCSa0vqO69@cluster0.kvpeegn.mongodb.net/Kodemia"
 
 mongoose.connect(URL)
@@ -56,17 +67,17 @@ mongoose.connect(URL)
     console.log("No conecto",error)
 })
 
-app.get ("/",(req,res)=> {
+app.get ("/",capsuladeMidd,(req,res)=> {
     res.json("Estamos en el home")
 })
 
 
 app.get ("/koders", async (request,response)=> {
     try {
-        // const {name} = request.query
-        // acceder a la bd
+        // * const {name} = request.query
+        // * acceder a la bd
         const koders = await Koder.find(request.query);
-        // console.log("koders",koders)
+        // * console.log("koders",koders)
         response.json({
             succes:true,
             data:koders
@@ -82,7 +93,7 @@ app.get ("/koders", async (request,response)=> {
 
 app.post("/koders", async (request,response)=> {
 try {
-// console.log("request:",request.body)
+// * console.log("request:",request.body)
     const koder = await  Koder.create(request.body)
     response.status(201);
     response.json ({
@@ -115,10 +126,65 @@ app.get ("/koders/:id", async (request,response)=> {
                 message:"El id es demasiado corto, vuelva a intentar"
             })
         } else {
-            response.status(418) // 404
+            response.status(418) // *404
             response.json({
                 succes:false,
                 message:"El koder que buscabas no existe"
+            })
+        }
+    }
+})
+
+app.delete ("/koders/:id", async (request,response)=>{
+    const {id} = request.params
+    let _succes = true;
+    try {
+        const deleteKoder = await Koder.findByIdAndDelete(id)
+        if (!deleteKoder) {
+            succes:false;
+            response.status(404)
+        }
+        console.log(deleteKoder)
+        response.status(200)
+        response.json ({
+            succes:_succes,
+            message:"koder eliminado"
+        })
+    } catch (error) {
+        response.status(404)
+        response.json({
+            succes:false,
+            message:error.message
+        })
+    }
+})
+
+app.patch("/koders/:id", async (req,res)=> {
+    const {id} = req.params
+    try {
+        const updateKoder = await Koder.findByIdAndUpdate(id,{name:"Ale modificada"})
+        if (id.length === 0) {
+            res.json ({
+                message:"No se ingreso un ID"
+            })
+        }
+        res.status(200);
+        res.json({
+            succes:true,
+            message:`El koder con el ID:${id}fue actualizado`,
+            data:updateKoder
+        })
+    } catch (error) {
+        if (id.length < 8) {
+            res.status(400)
+            res.json ({
+                succes:false,
+                message:"El id es demasiado corto, vuelva a intentar"
+            })
+        } else {
+            res.status(404)
+            res.json ({
+                message:"El ID ingresado no corresponde con ningun Koder"
             })
         }
     }
